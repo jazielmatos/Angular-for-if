da9b31f0131d1d71b0ca4c54ff92f08653777593
jazielmatos <jazielmatos82@gmail.com>
2022-11-14 21:15:19 -0300

    Projeto rotas

---
+++ src/app/paginas/tabelas/tabelas.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tabelas',
+  templateUrl: './tabelas.component.html',
+  styleUrls: ['./tabelas.component.css']
+})
+export class TabelasComponent implements OnInit {
+
+  public produtos:any[] = []
+  
+  constructor() { 
+   
+
+  
+  }
+
+  ngOnInit(): void {
+    this.produtos.push(
+      {
+        id: 1,
+        nome: 'Morango',
+        descricao: 'Um Morango',
+        preco: 5.5
+      },
+      {
+        id: 2,
+        nome: 'Pessego',
+        descricao: 'Um Pessego',
+        preco: 5.5
+      }
+    )
+
+    
+  }
+
+}
